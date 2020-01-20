b94115f461c8ed6f6c085afeadcae01b13137eb6
陈洁 <chenyu530324@163.com>
2020-01-20 17:52:37 +0800

    Added gradle basic configuration

--- src/test/java/spring/boot/template/ApplicationTest.java
+++ src/test/java/spring/boot/template/ApplicationTest.java
@@ -4,7 +4,8 @@
 package spring.boot.template;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertNotNull;
 
 public class ApplicationTest {
     @Test public void testAppHasAGreeting() {
